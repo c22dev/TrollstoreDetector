dcfc31d88a1a6f8a22fdb9f3ccc330b1f6418457
c22dev <constclerc@gmail.com>
2022-12-13 00:30:07 +0100

    Fixed keep resume

--- iOSTrollstoreDetection/ContentView.swift
+++ iOSTrollstoreDetection/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+//This is your own ContentView. No need to replace anything there. Go in the other swift file.
 struct ContentView: View {
     var body: some View {
         VStack {
